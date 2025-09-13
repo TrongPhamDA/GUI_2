a26b325bb12912e55fb7f0e2f15fcc414da2b76f
trong.phamngoc <trong.phamngoc@kcc.com>
2025-09-13 17:58:45 +0700

    Update myfunctions.py

--- pages/myfunctions.py
+++ pages/myfunctions.py
@@ -1077,6 +1077,7 @@ def fn_display_hotel_insights(selected_hotel_id, df_hotels,figsize=DEFAULT_FIGSI
                     else:
                         st.info("No positive keywords found for all hotels")
                 
+                st.write("---")
                 # Row 2: Negative Keywords
                 # st.markdown("**Negative Keywords**")
                 col3, col4 = st.columns(2)
